10ff45455f2785783d79ae9626cebe62d6e4d7df
Your Name <you@example.com>
2025-12-17 11:08:52 -0500

    2 way get data

--- src/Components/FetchData.jsx
+++ src/Components/FetchData.jsx
@@ -1,13 +1,27 @@
 import React from 'react'
+import UseFetch from './UseFetch';
+import fruitData from './fruit.json';
 
 const FetchData = () => {
-  return (
-    <>
-     <ul className='list_data_main'>
+    //const APIurl='https://api.npoint.io/9045c260b1565daa9e15'
+    //const [data]=UseFetch(APIurl);
+    const data=fruitData;
+    return (
+        <>
         <h1 className='usefetch_heading'>Use Fetch Custom Hook</h1>
-     </ul>
-    </>
-  )
+        <ul className='list_data_main'>
+            {data && data.map((e, index)=>(
+            <li key={index} className='list_data'>
+                <h3>{e.name}</h3>
+                <p><img src={e.image} alt={e.name} style={{width:"200px"}} /></p>
+                <p><strong>Importance: </strong>{e.importance}</p>
+                <p><strong>Benefits: </strong>{e.benefits}</p>
+                <p><strong>Time to eat: </strong>{e.best_time_to_intake}</p>    
+            </li>
+            ))}
+        </ul>
+        </>
+    )
 }
 
 export default FetchData
--- src/Components/UseFetch.jsx
+++ src/Components/UseFetch.jsx
@@ -1,5 +1,17 @@
+import React, {useState, useEffect } from "react";
+
 const UseFetch = (url) => {
-    
+    const[data,setData]=useState();  
+    useEffect(()=>{
+        if (!url) return;
+        fetch(url).then((res)=>res.json())
+        .then((data)=>setData(data))
+        .catch((error) => {
+            console.error('Fetch error:', error);
+            setData(null);
+      });
+    },[url])
+    return [data]  
 }
 
 export default UseFetch
